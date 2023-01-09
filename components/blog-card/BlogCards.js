5c0fdcb6fbbb980554b3df333f0473ad157660c0
Subhan-Akram <subhan@denovers.com>
2023-01-09 20:27:02 +0500

    google map added

--- components/blog-card/BlogCards.js
+++ components/blog-card/BlogCards.js
@@ -1,21 +1,27 @@
 import Image from 'next/image'
 import React from 'react'
 import c from "../../images/c2.jpeg"
+import Link from 'next/link'
 function BlogCards({data}) {
 
   return (
-    <div className='blog_card_component  bd_red'>
-         <div className='img_div bd_red'><Image src={c} /></div>
+   
+    <div className='blog_card_component  bd_blue'>
+ 
+         <div className='img_div bd_red blog_card_img'><Image src={c} /></div>
          <br />
         <div> <h1 className='blog_card_title bd_red section1_title'>{data?.title}</h1></div>
         <div>
+       
         <p className='blog_card_text text_sm'>
              {data?.description}
-             
+          
          </p>
-       
+  
         </div>
+          
     </div>
+ 
   )
 }
 
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,4 +1,4 @@
-
+import Reacr,{useState,useEffect, useRef} from "react"
 import Link from 'next/link'
 import Container from '../../components/container'
 import distanceToNow from '../../lib/dateRelative'
@@ -7,31 +7,54 @@ import { fetchEntries } from '../api/comment'
 import BlogCards from '../../components/blog-card/BlogCards'
 import c1 from "../../images/c2.jpeg"
 import Image from 'next/image'
+import { FaRocket } from 'react-icons/fa'
+// import { ImRocket} from 'react-icons/io'
 
 export default function NotePage({posts}) {
+  let ref=useRef(null)
   console.log("posts>> ",posts)
+  const [search,setSearch]=useState("");
+  const [filterPost,setFilterPost]=useState([])
+  useEffect(()=>{
+    setFilterPost(posts)
+  },[])
+
+
+  const filterBlogs=(s)=>{
+    let inputValue=ref?.current.value.toLowerCase()
+console.log("click",inputValue)
+if(inputValue == ""){
+     return  setFilterPost(posts)
+}
+const filteredItems = posts.filter(item => item.title.toLowerCase().includes(inputValue));
+setFilterPost(filteredItems);
+  }
   return (
     <Container>
+      
   <div className='container_home bd_red'>
   
 
-
+       <div className='blog_search bd_red'>
+         <input ref={ref} className='bd_yellow' placeholder='search' onChange={()=>{filterBlogs()}}></input>
+         <button className='bd_blue search_icon' onClick={()=>{filterBlogs("f")}}><FaRocket /></button>
+       </div>
 
          <div className='blog_cards bd_red'>
-      {posts.map((post) => (
-          <article key={post.title} className="mb-10">
+      {filterPost.map((post) => (
+          // <article key={post.title} className="mb-10">
             <Link
               as={`/posts/${post.slug}`}
               href="/posts/[slug]"
-              className="text-lg leading-6 font-bold"
+              className="blog_link bd_yellow text-lg leading-6 font-bold"
             >
-                     <div className='blog_cards'>
+                
                         <BlogCards data={post} />
    
-                      </div>
-            </Link>
+                 
+             </Link>
          
-          </article>
+          // </article>
         
         ))}
         </div> 
