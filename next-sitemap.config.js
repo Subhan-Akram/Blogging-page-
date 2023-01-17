d4b2495f72ade24b8909481609c18c4e131f4c0e
Subhan-Akram <subhan@denovers.com>
2023-01-17 17:37:58 +0500

    site map changed

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://blogging-page-psi.vercel.app',
+  siteUrl:'https://blogging-page-psi.vercel.app',
   generateRobotsTxt: true, // (optional),
   outDir:"./out"
   // ...other options
